210e965f4a2a2003273d8f8329120318d893f840
Robert Ciliberto <rciliberto2@bostonk12.org>
2017-01-20 09:39:49 -0500

    Added copyright information

--- src/lecture_04/Lab1.java
+++ src/lecture_04/Lab1.java
@@ -1,3 +1,20 @@
+/*	lecture 4 lab, AP Computer Science
+ * 
+ *  Copyright (C) 2017  Robert Ciliberto
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package lecture_04;
 
 public class Lab1 {
@@ -5,10 +22,10 @@ public class Lab1 {
 		double height=70;	//Store height (in inches) in a double
 		double weight=194.25;	//Store weight (in pounds) in a double
 		
-		System.out.println("Height (in inches): " + height);
-		System.out.println("Weight (in pounds): " + weight);
-		System.out.println("BMI = " + bmi(height,weight));
-		System.out.println(weightClass(height,weight));
+		System.out.println("Height (in inches): " + height);	//Print height
+		System.out.println("Weight (in pounds): " + weight);	//Print weight
+		System.out.println("BMI = " + bmi(height,weight));	//Print BMI
+		System.out.println(weightClass(height,weight));	//Print weight class
 	}
 	
 	public static double bmi(double height, double weight){	//Accepts a double for height in inches and weight in pounds
--- src/lecture_15/Student.java
+++ src/lecture_15/Student.java
@@ -1,3 +1,20 @@
+/* 	lecture 15 lab, AP Computer Science
+ * 
+ *  Copyright (C) 2017  Robert Ciliberto
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package lecture_15;
 public class Student{
 	private String name, grade;
