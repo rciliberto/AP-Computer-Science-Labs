bd635fdd55f01a821e426da2650ef6390e44b4b5
David Shen <PantherMan594@users.noreply.github.com>
2017-01-31 14:13:40 -0500

    Create QuestionDriver.java

---
+++ src/lecture_19/QuestionDriver.java
@@ -0,0 +1,9 @@
+package lecture_19;
+
+import java.util.*;
+
+public class QuestionDriver {
+  public static void main(String[] args) {
+    Question question = new Question("Who is the first president of the United States?", true, "George Washington");
+  }
+}
