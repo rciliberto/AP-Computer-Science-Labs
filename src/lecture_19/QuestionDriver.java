8494e9540003081579e139db809a51dd421b5436
David Shen <PantherMan594@users.noreply.github.com>
2017-01-31 14:44:58 -0500

    Update QuestionDriver.java

--- src/lecture_19/QuestionDriver.java
+++ src/lecture_19/QuestionDriver.java
@@ -25,13 +25,13 @@ public class QuestionDriver {
   public static void main(String[] args) {
     Question question = new Question("Who is the first president of the United States?", true, "George Washington");
     question.addChoice("Abraham Lincoln");
-    question.addChoice("Franklin Roosevelt");
+    System.out.println(question.addChoice("Franklin Roosevelt"));
     question.addChoice(1, "George Washington");
     System.out.println(question);
     question.printAnswer();
     
     Question question2 = new Question("5x+12 = 17\nx=?", false, "1");
-    question2.addChoice("2");
+    System.out.println(question2.addChoice("2"));
     System.out.println(question2);
     question2.printAnswer();
   }
