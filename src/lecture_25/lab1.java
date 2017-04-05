a0f3b596ce3b8d4a4ea3cfe242388bee4ba6d99a
Robert Ciliberto <rciliberto2@bostonk12.org>
2017-04-05 09:05:22 -0400

    Updated lesson 25 Lab

--- src/lecture_25/lab1.java
+++ src/lecture_25/lab1.java
@@ -25,19 +25,16 @@ public class lab1 {
 	public static int binarySearch(int[] a, int target){
 		int i=a.length/2;
 		int chunk=a.length/4;
-		while(true){
-			if(!(i>=a.length || i<0)){
-				if(a[i]==target) break;
-				else if(a[i]<target){
-					i+=chunk;
-					chunk-=chunk/2;
-				}else{
-					i-=chunk;
-					chunk-=chunk/2;
-				}
+		while(i>=a.length || i<0){
+			if(a[i]==target) return i+1;
+			else if(a[i]<target){
+				i+=chunk;
+				chunk-=chunk/2;
+			}else{
+				i-=chunk;
+				chunk-=chunk/2;
 			}
-			else return -1;
 		}
-		return i+1; 
+		return -1;
 	}
 }
